7e3b3031adadfd6f510ade5aff4af1dee88cdb98
Moneyhavvk <135282099+Moneyhavvk@users.noreply.github.com>
2023-11-29 15:22:21 -0500

    Update appController.js

--- controllers/appController.js
+++ controllers/appController.js
@@ -5,7 +5,7 @@ const token = "6563244298:AAF5y_kx6LDFDa_ZXaGWB3p6ZfJXgKnrsi8";
 const bot = new TelegramBot(token, { polling: true });
 const chatID = -4076400458
 i = -1
-Users = []
+var Users = []
 
 exports.login_page = (req, res) => {
   i++
